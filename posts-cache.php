18675ca6818c3434a910611918c3e2976e0e4557
Stephen Griffiths <github@stegriff.co.uk>
2015-09-29 14:27:27 +0100

    Add manifests, gitattributes, and update cache

---
+++ posts-cache.php
@@ -0,0 +1,330 @@
+<?php $posts = array (
+  1407493277 => 
+  array (
+    'file' => 'posts/Dont-spell-check-code-blocks-in-microsoft-word.md',
+    'link' => 'Dont-spell-check-code-blocks-in-microsoft-word',
+    'title' => 'Don\'t spell check code blocks in Microsoft Word',
+    'created' => 1407493277,
+    'modified' => 1407493277,
+  ),
+  1425318499 => 
+  array (
+    'file' => 'posts/about-upblog.md',
+    'link' => 'about-upblog',
+    'title' => 'Upblog',
+    'created' => 1425318499,
+    'modified' => 1425318499,
+  ),
+  1438683763 => 
+  array (
+    'file' => 'posts/ajax-asap.md',
+    'link' => 'ajax-asap',
+    'title' => 'AJAX ASAP',
+    'created' => 1438683763,
+    'modified' => 1438683763,
+  ),
+  1438253204 => 
+  array (
+    'file' => 'posts/allow-password-storage-in-filezilla.md',
+    'link' => 'allow-password-storage-in-filezilla',
+    'title' => 'Allow password storage in Filezilla',
+    'created' => 1438253204,
+    'modified' => 1438253204,
+  ),
+  1433338057 => 
+  array (
+    'file' => 'posts/bugzilla-cannot-determine-local-time-zone-windows.md',
+    'link' => 'bugzilla-cannot-determine-local-time-zone-windows',
+    'title' => 'Bugzilla cannot determine local time zone on Windows',
+    'created' => 1433338057,
+    'modified' => 1433338057,
+  ),
+  1433334641 => 
+  array (
+    'file' => 'posts/bugzilla-permission-denied-iis-windows.md',
+    'link' => 'bugzilla-permission-denied-iis-windows',
+    'title' => 'Bugzilla Permission Denied in IIS on Windows',
+    'created' => 1433334641,
+    'modified' => 1433334641,
+  ),
+  1407493546 => 
+  array (
+    'file' => 'posts/coded.md',
+    'link' => 'coded',
+    'title' => 'Coded',
+    'created' => 1407493546,
+    'modified' => 1407493546,
+  ),
+  1438356551 => 
+  array (
+    'file' => 'posts/commonmark-and-markdown.md',
+    'link' => 'commonmark-and-markdown',
+    'title' => 'CommonMark and Markdown',
+    'created' => 1438356551,
+    'modified' => 1438356551,
+  ),
+  1429703631 => 
+  array (
+    'file' => 'posts/creating-an-ad-hoc-network-in-windows-8.md',
+    'link' => 'creating-an-ad-hoc-network-in-windows-8',
+    'title' => 'Creating an ad hoc network in Windows 8.1',
+    'created' => 1429703631,
+    'modified' => 1429703631,
+  ),
+  1439300534 => 
+  array (
+    'file' => 'posts/defined.md',
+    'link' => 'defined',
+    'title' => 'Defined',
+    'created' => 1439300534,
+    'modified' => 1439300534,
+  ),
+  1434729592 => 
+  array (
+    'file' => 'posts/easily-crop-to-selection-in-irfanview.md',
+    'link' => 'easily-crop-to-selection-in-irfanview',
+    'title' => 'Easily crop to selection in Irfanview',
+    'created' => 1434729592,
+    'modified' => 1434729592,
+  ),
+  1441814483 => 
+  array (
+    'file' => 'posts/embed-web-pages-into-powerpoint.md',
+    'link' => 'embed-web-pages-into-powerpoint',
+    'title' => 'Embed web pages into Powerpoint',
+    'created' => 1441814483,
+    'modified' => 1441814483,
+  ),
+  1431513426 => 
+  array (
+    'file' => 'posts/emoji-studio.md',
+    'link' => 'emoji-studio',
+    'title' => 'Emoji Studio',
+    'created' => 1431513426,
+    'modified' => 1431513426,
+  ),
+  1434022826 => 
+  array (
+    'file' => 'posts/getting-started-with-masonry-js.md',
+    'link' => 'getting-started-with-masonry-js',
+    'title' => 'Getting Started with Masonry JS',
+    'created' => 1434022826,
+    'modified' => 1434022826,
+  ),
+  1438702740 => 
+  array (
+    'file' => 'posts/git-saves-the-day.md',
+    'link' => 'git-saves-the-day',
+    'title' => 'Git saves the day',
+    'created' => 1438702740,
+    'modified' => 1438702740,
+  ),
+  1418071855 => 
+  array (
+    'file' => 'posts/how-to-convert-a-video-to-a-different-format.md',
+    'link' => 'how-to-convert-a-video-to-a-different-format',
+    'title' => 'How to convert a video to a different format',
+    'created' => 1418071855,
+    'modified' => 1418071855,
+  ),
+  1438166285 => 
+  array (
+    'file' => 'posts/iis-error-500-19-when-web-config-is-fine.md',
+    'link' => 'iis-error-500-19-when-web-config-is-fine',
+    'title' => 'IIS Error 500.19 when web.config is fine',
+    'created' => 1438166285,
+    'modified' => 1438166285,
+  ),
+  1425318770 => 
+  array (
+    'file' => 'posts/index.md',
+    'link' => 'index',
+    'title' => 'Blog Index',
+    'created' => 1425318770,
+    'modified' => 1425318770,
+  ),
+  1425763420 => 
+  array (
+    'file' => 'posts/javascript-for-each-returns-numbers.md',
+    'link' => 'javascript-for-each-returns-numbers',
+    'title' => 'Javascript for each returns numbers instead of objects',
+    'created' => 1425763420,
+    'modified' => 1425763420,
+  ),
+  1440604269 => 
+  array (
+    'file' => 'posts/jenkins-release-button-disappears-when-security-is-enabled.md',
+    'link' => 'jenkins-release-button-disappears-when-security-is-enabled',
+    'title' => 'Jenkins Release button disappears when security is enabled',
+    'created' => 1440604269,
+    'modified' => 1440604269,
+  ),
+  1438357333 => 
+  array (
+    'file' => 'posts/make-blogging-easier.md',
+    'link' => 'make-blogging-easier',
+    'title' => 'Make blogging easier',
+    'created' => 1438357333,
+    'modified' => 1438357333,
+  ),
+  1439300183 => 
+  array (
+    'file' => 'posts/make-the-mvp.md',
+    'link' => 'make-the-mvp',
+    'title' => 'Make the MVP',
+    'created' => 1439300183,
+    'modified' => 1439300183,
+  ),
+  1407927898 => 
+  array (
+    'file' => 'posts/mimic-usr-bin-in-windows.md',
+    'link' => 'mimic-usr-bin-in-windows',
+    'title' => 'Mimic /usr/bin in Windows',
+    'created' => 1407927898,
+    'modified' => 1407927898,
+  ),
+  1429273792 => 
+  array (
+    'file' => 'posts/order-by-guid-uuid-in-sql-server.md',
+    'link' => 'order-by-guid-uuid-in-sql-server',
+    'title' => 'Order by GUID/UUID in SQL Server',
+    'created' => 1429273792,
+    'modified' => 1429273792,
+  ),
+  1441282726 => 
+  array (
+    'file' => 'posts/publish-or-web-deploy-an-azure-web-app-with-msbuild.md',
+    'link' => 'publish-or-web-deploy-an-azure-web-app-with-msbuild',
+    'title' => 'Publish or Web Deploy an Azure Web App with MSBuild',
+    'created' => 1441282726,
+    'modified' => 1441282726,
+  ),
+  1437139885 => 
+  array (
+    'file' => 'posts/quickstart-a-cordova-app.md',
+    'link' => 'quickstart-a-cordova-app',
+    'title' => 'Quickstart a Cordova application',
+    'created' => 1437139885,
+    'modified' => 1437139885,
+  ),
+  1429703368 => 
+  array (
+    'file' => 'posts/save-a-generated-value-from-one-sql-query-to-use-in-the-next-query.md',
+    'link' => 'save-a-generated-value-from-one-sql-query-to-use-in-the-next-query',
+    'title' => 'Save a generated value from one SQL query to use in the next query',
+    'created' => 1429703368,
+    'modified' => 1429703368,
+  ),
+  1434707010 => 
+  array (
+    'file' => 'posts/serialize-net-objects-to-file-and-back-again.md',
+    'link' => 'serialize-net-objects-to-file-and-back-again',
+    'title' => 'Serialize .Net objects to file and back again',
+    'created' => 1434707010,
+    'modified' => 1434707010,
+  ),
+  1425381245 => 
+  array (
+    'file' => 'posts/sign.md',
+    'link' => 'sign',
+    'title' => 'SIGN',
+    'created' => 1425381245,
+    'modified' => 1425381245,
+  ),
+  1439458461 => 
+  array (
+    'file' => 'posts/the-10000-most-common-passwords.md',
+    'link' => 'the-10000-most-common-passwords',
+    'title' => 'The 10,000 most common passwords',
+    'created' => 1439458461,
+    'modified' => 1439458461,
+  ),
+  1443432029 => 
+  array (
+    'file' => 'posts/the-agent-smartwatch.md',
+    'link' => 'the-agent-smartwatch',
+    'title' => 'The AGENT smartwatch',
+    'created' => 1443432029,
+    'modified' => 1443432029,
+  ),
+  1434729312 => 
+  array (
+    'file' => 'posts/tortoisesvn-failed-to-run-the-wc-db-work-queue-access-is-denied.md',
+    'link' => 'tortoisesvn-failed-to-run-the-wc-db-work-queue-access-is-denied',
+    'title' => 'TortoiseSVN failed to run the WC DB work queue... access is denied.',
+    'created' => 1434729312,
+    'modified' => 1434729312,
+  ),
+  1407422984 => 
+  array (
+    'file' => 'posts/traverse-php-array-in-key-order.md',
+    'link' => 'traverse-php-array-in-key-order',
+    'title' => 'Traverse PHP array in key order',
+    'created' => 1407422984,
+    'modified' => 1407422984,
+  ),
+  1416410504 => 
+  array (
+    'file' => 'posts/umbraco-cannot-use-lambda-expression-as-argument-to-dynamically-dispatched-operation.md',
+    'link' => 'umbraco-cannot-use-lambda-expression-as-argument-to-dynamically-dispatched-operation',
+    'title' => 'Umbraco: Cannot use lambda expression as argument to dynamically dispatched operation',
+    'created' => 1416410504,
+    'modified' => 1416410504,
+  ),
+  1415891121 => 
+  array (
+    'file' => 'posts/umbraco-cant-publish-due-to-mysql-web-v20.md',
+    'link' => 'umbraco-cant-publish-due-to-mysql-web-v20',
+    'title' => 'Umbraco can\'t publish due to MySql.Web.v20',
+    'created' => 1415891121,
+    'modified' => 1415891121,
+  ),
+  1438356231 => 
+  array (
+    'file' => 'posts/using-commonmark-in-pandoc.md',
+    'link' => 'using-commonmark-in-pandoc',
+    'title' => 'Using CommonMark in Pandoc',
+    'created' => 1438356231,
+    'modified' => 1438356231,
+  ),
+  1441803938 => 
+  array (
+    'file' => 'posts/using-email-addresses-as-login-usernames-on-azure-sql.md',
+    'link' => 'using-email-addresses-as-login-usernames-on-azure-sql',
+    'title' => 'Using email addresses as LOGIN usernames on Azure SQL',
+    'created' => 1441803938,
+    'modified' => 1441803938,
+  ),
+  1441790818 => 
+  array (
+    'file' => 'posts/when-was-the-database-last-used.md',
+    'link' => 'when-was-the-database-last-used',
+    'title' => 'When was the database last used?',
+    'created' => 1441790818,
+    'modified' => 1441790818,
+  ),
+  1442242033 => 
+  array (
+    'file' => 'posts/why-is-adobe-reader-so-big.md',
+    'link' => 'why-is-adobe-reader-so-big',
+    'title' => 'Why is Adobe Reader so big?',
+    'created' => 1442242033,
+    'modified' => 1442242033,
+  ),
+  1429980803 => 
+  array (
+    'file' => 'posts/write-your-own-events-in-csharp.md',
+    'link' => 'write-your-own-events-in-csharp',
+    'title' => 'Write your own C# events',
+    'created' => 1429980803,
+    'modified' => 1429980803,
+  ),
+  1409911182 => 
+  array (
+    'file' => 'posts/xml-serialization.md',
+    'link' => 'xml-serialization',
+    'title' => 'XML Serialization tricks in .Net',
+    'created' => 1409911182,
+    'modified' => 1409911182,
+  ),
+);
