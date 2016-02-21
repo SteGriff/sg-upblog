5601f2295305ab71dea3293573dcfec2b729da1f
Ste Griffiths <github@stegriff.co.uk>
2016-02-21 17:40:26 +0000

    Fix class on footer

--- templates/bits/footer.php
+++ templates/bits/footer.php
@@ -1,5 +1,5 @@
 	<footer>
-		<p><time class="year">2014</time> SteGriff - Stephen Griffiths</p>
+		<p class="wispy"><time class="year">2014</time> SteGriff - Stephen Griffiths</p>
 	</footer>
 	
 	<script src="http://code.jquery.com/jquery-2.2.0.min.js"></script>
